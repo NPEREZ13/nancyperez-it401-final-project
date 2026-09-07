f9a770f9702cf5148a7b43726b61a5b20a12211b
Nancy Perez <nnancypperez04@gmail.com>
2026-09-06 21:11:56 -0700

    Coffee Shop Changes

--- routes/main.py
+++ routes/main.py
@@ -5,3 +5,7 @@ def register_routes(app):
     @app.route("/")
     def index():
         return render_template("index.html")
+
+    @app.route("/menu")
+    def menu():
+        return render_template("menu.html")
